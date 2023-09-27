cbd926dc546d524114c2af23067a1e26982208f5
kadun1 <kadunjj@gmail.com>
2023-09-28 02:50:10 +0900

    kopring : issue crud

--- issue-service/src/main/kotlin/example/kopring/issueservice/domain/IssueRepository.kt
+++ issue-service/src/main/kotlin/example/kopring/issueservice/domain/IssueRepository.kt
@@ -1,6 +1,9 @@
 package example.kopring.issueservice.domain
 
+import example.kopring.issueservice.domain.enums.IssueStatus
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface IssueRepository : JpaRepository<Issue, Long>{
+interface IssueRepository : JpaRepository<Issue, Long> {
+
+    fun findAllByStatusOrderByCreatedAtDesc(status: IssueStatus) : List<Issue>?
 }
--- issue-service/src/main/kotlin/example/kopring/issueservice/service/IssueService.kt
+++ issue-service/src/main/kotlin/example/kopring/issueservice/service/IssueService.kt
@@ -2,8 +2,11 @@ package example.kopring.issueservice.service
 
 import example.kopring.issueservice.domain.Issue
 import example.kopring.issueservice.domain.IssueRepository
+import example.kopring.issueservice.domain.enums.IssueStatus
+import example.kopring.issueservice.exception.NotFoundException
 import example.kopring.issueservice.model.IssueRequest
 import example.kopring.issueservice.model.IssueResponse
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -26,5 +29,34 @@ class IssueService(
 
         return IssueResponse(issueRepository.save(issue))
 
+    }
+
+    @Transactional(readOnly = true)
+    fun getAll(status: IssueStatus) =
+        issueRepository.findAllByStatusOrderByCreatedAtDesc(status)
+            ?.map { IssueResponse(it) }
+
+    fun get(id: Long): IssueResponse {
+        val issue = issueRepository.findByIdOrNull(id) ?: throw NotFoundException("이슈가 존재하지 않습니다")
+        return IssueResponse(issue)
+    }
+
+    @Transactional
+    fun edit(userId: Long, id: Long, request: IssueRequest) : IssueResponse {
+        val issue = issueRepository.findByIdOrNull(id) ?: throw NotFoundException("이슈가 존재하지 않습니다")
+
+        return with(issue){
+            summary = request.summary
+            description = request.description
+            this.userId= userId
+            type = request.type
+            priority = request.priority
+            status = request.status
+            IssueResponse(issueRepository.save(this))
+        }
+    }
+
+    fun delete(id: Long) {
+        issueRepository.deleteById(id)
     }
 }
